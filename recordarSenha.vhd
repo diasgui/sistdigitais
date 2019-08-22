20d8287daf1b535323cfd15f1efc46db036441d2
Guilherme Dias <31046799+diasgui@users.noreply.github.com>
2019-08-22 18:17:24 -0300

    Update recordarSenha.vhd

--- recordarSenha.vhd
+++ recordarSenha.vhd
@@ -1,28 +1,86 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
--- esboço da operacao de recordar senha
-entity recordarSenha is
-	port(
-    	senha_digitada: in std_logic_vector(0 to 3);
-        senha_salva: in std_logic_vector(0 to 3);
-        qtd_digitos: out integer
-        );
-end entity recordarSenha;
+-- esboço da operacao de abrir cofre
+entity abrirCofre is
+  port(
+    chaves: in std_logic_vector(0 to 3);
+    botaoSet: in std_logic;
+    qtd_digitos: out integer
+    );
+end entity abrirCofre;
 
-architecture comportamental of recordarSenha is
+architecture comportamental of abrirCofre is
+  type State_type is (op0, op3, d0, d1, d2, d3, final)
+  
+  signal y: State_type
+  
+  signal senha_salva: std_logic_vector(0 to 15);
+  signal senha_binaria: std_logic_vector(0 to 15);
 begin
-	qtd_digitos <= 0;
-	
-	-- senhas digitada e salva em inteiros (bcd)
-	for count in 0 to 3 loop
-    	if senha_digitada(count) == senha_salva(0)
-        	qtd_digitos <= qtd_digitos +1;
-        elsif senha_digitada == senha_salva(1)
-        	qtd_digitos <= qtd_digitos +1;
-        elsif senha_digitada == senha_salva(2)
-        	qtd_digitos <= qtd_digitos +1;
-        end if;
-    end loop;                                     
+
+  -- inicialmente a senha esta sendo digitada e armazenada em binario, necessario converter para inteiro
+  process(botaoSet)
+  begin
+    if botaoSet = '0' then
+      y <= op3;
+    else if rising_edge(botaoSet) then
+      case y
+        when op3 =>
+       	  if chaves(0 to 3) == senha_salva(0 to 3) then
+            qtd_digitos <= qtd_digitos + 1;
+          elsif chaves(0 to 3) == senha_salva(4 to 7) then
+            qtd_digitos <= qtd_digitos + 1;
+          elsif chaves(0 to 3) == senha_salva(8 to 11) then
+            qtd_digitos <= qtd_digitos + 1;
+          elsif chaves(0 to 3) == senha_salva(12 to 15) then
+            qtd_digitos <= qtd_digitos + 1;
+          end if;
+          
+          y <= d0;
+        when d0 =>
+          if chaves(0 to 3) == senha_salva(0 to 3) then
+            qtd_digitos <= qtd_digitos + 1;
+          elsif chaves(0 to 3) == senha_salva(4 to 7) then
+            qtd_digitos <= qtd_digitos + 1;
+          elsif chaves(0 to 3) == senha_salva(8 to 11) then
+            qtd_digitos <= qtd_digitos + 1;
+          elsif chaves(0 to 3) == senha_salva(12 to 15) then
+            qtd_digitos <= qtd_digitos + 1;
+          end if;
+          
+          d0 <= d1;
+        when d1 =>
+          if chaves(0 to 3) == senha_salva(0 to 3) then
+            qtd_digitos <= qtd_digitos + 1;
+          elsif chaves(0 to 3) == senha_salva(4 to 7) then
+            qtd_digitos <= qtd_digitos + 1;
+          elsif chaves(0 to 3) == senha_salva(8 to 11) then
+            qtd_digitos <= qtd_digitos + 1;
+          elsif chaves(0 to 3) == senha_salva(12 to 15) then
+            qtd_digitos <= qtd_digitos + 1;
+          end if;
+          
+          d1 <= d2;
+        when d2 =>
+          if chaves(0 to 3) == senha_salva(0 to 3) then
+            qtd_digitos <= qtd_digitos + 1;
+          elsif chaves(0 to 3) == senha_salva(4 to 7) then
+            qtd_digitos <= qtd_digitos + 1;
+          elsif chaves(0 to 3) == senha_salva(8 to 11) then
+            qtd_digitos <= qtd_digitos + 1;
+          elsif chaves(0 to 3) == senha_salva(12 to 15) then
+            qtd_digitos <= qtd_digitos + 1;
+          end if;
+          
+          d2 <= d3;
+        when d3 =>
+          -- acender leds qtd_ditigos
+          y <= final;
+        when final =>
+       	  y <= op0;
+      end case;
+    end if;
+  end process;
 
 end comportamental;
