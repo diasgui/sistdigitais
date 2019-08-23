123187a9374f69eb4862bf3cc724315245d38564
Guilherme Dias <31046799+diasgui@users.noreply.github.com>
2019-08-23 15:28:38 -0300

    Update BCD.vhd

--- BCD.vhd
+++ BCD.vhd
@@ -3,24 +3,24 @@ use IEEE.std_logic_1164.all;
 
 entity BCD is
 	port(
-    	binary: in std_logic_vector(3 downto 0);
-        decimal: out integer
-        );
+    	binario: in std_logic_vector(3 downto 0);
+		inteiro: out integer
+	);
 end entity BCD;
 
 architecture comportamental of BCD is
 begin
-	case binary is
-    	when "0000" => decimal <= '0';
-        when "0001" => decimal <= '1';
-        when "0010" => decimal <= '2';
-        when "0011" => decimal <= '3';
-        when "0100" => decimal <= '4';
-        when "0101" => decimal <= '5';
-        when "0110" => decimal <= '6';
-        when "0111" => decimal <= '7';
-        when "1000" => decimal <= '8';
-        when "1001" => decimal <= '9';
-        when others => decimal <= 'X';
-	end case;
+	with binario select
+		inteiro <=
+			0 when "0000",
+			1 when "0001",
+			2 when "0010",
+			3 when "0011",
+			4 when "0100",
+			5 when "0101",
+			6 when "0110",
+			7 when "0111",
+			8 when "1000",
+			9 when "1001",
+			-1 when others;
 end comportamental;
